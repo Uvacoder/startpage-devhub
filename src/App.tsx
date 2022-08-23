81ef34fd5c92ecd6daf2c3f32999c578c5ef99a7
Ilya Sudakov <ilyasudakov@inbox.ru>
2022-08-23 15:04:39 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return <div>App</div>;
+}
+
+const TimeTitle = () => {
+  return (
+    <div className="flex justify-between">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
