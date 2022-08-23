431071ad0314758f186b8ea4d4346d74b405bd93
Ilya Sudakov <ilyasudakov@inbox.ru>
2022-08-23 20:10:36 +0300

    Add persistance with redux-persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,15 +2,27 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import todoReducer from '../components/ToDoList/todoReducer';
 import linksReducer from '../components/UserLinks/linksReducer';
 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 const rootReducer = combineReducers({
   todos: todoReducer,
   links: linksReducer
 });
 
+const persistedReducer = persistReducer(
+  {
+    key: 'root',
+    storage
+  },
+  rootReducer
+);
+
 const store = configureStore({
-  reducer: rootReducer
+  reducer: persistedReducer
 });
 
 export default store;
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
